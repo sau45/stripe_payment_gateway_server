5c0eab3a436dc447336df270b7ad5996abec99a5
Saurav Bharti <sauravbharti38@gmail.com>
2024-03-11 15:32:15 +0530

    minor change

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         payment_method_types:["card"],
         line_items:lineItems,
         mode:"payment",
-        success_url:`${process.env.FRONTEND_URL}/sucess`,
-        cancel_url:`${process.env.FRONTEND_URL}/cancel`,
+        success_url:`http://localhost:3000/sucess`,
+        cancel_url:"http://localhost:3000/cancel",
     });
 
     res.json({id:session.id})
